0042ff8fc560ad197e665d53fe69b80a969124e6
Hoon Lee <yl.hoony@gmail.com>
2017-12-04 17:17:52 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,9 +1,9 @@
 def starts_with_a_vowel?(word)
-  !!word.match(/\A[aeiou]/)
+  word.match(/[aeiouAEIOU]\w+/)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  text.match(/\A[un]/)
+  text.match(/[un]\w+[ing]/)
 end
 
 def words_five_letters_long(text)
