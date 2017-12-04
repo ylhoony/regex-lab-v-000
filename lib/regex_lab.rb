80ab9be54c1267a6d84220f029e740fe3ab1e07a
Hoon Lee <yl.hoony@gmail.com>
2017-12-04 17:13:07 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,13 +1,13 @@
 def starts_with_a_vowel?(word)
-
+  word[0].match(/[aeiou]/)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.match(/\A[un]/)
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\w{5}/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
